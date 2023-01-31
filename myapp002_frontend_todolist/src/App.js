e5586c86703aa4f24f18d8db20a4e1e4a4c9d501
viperact <viperact26@gmail.com>
2023-01-31 21:43:22 +0900

    update

--- myapp002_frontend_todolist/src/App.js
+++ myapp002_frontend_todolist/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const wrap = {
@@ -8,7 +8,74 @@ function App() {
     margin: '10px auto',
   };
 
-  return <div className='App' style={wrap}></div>;
+  let boardList = [
+    { id: 1, todoname: '운동하기', completed: 0 },
+    { id: 2, todoname: 'SNS꾸미기', completed: 0 },
+    { id: 3, todoname: '사진정리하기', completed: 0 },
+  ];
+
+  const [todos, setTodos] = useState([...boardList]);
+  const [input, setInput] = useState('');
+
+  const insertTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      { id: todos.length + 1, todoname: input, completed: 0 },
+      ...todos,
+    ]);
+
+    setInput(''); // 입력란 초기화
+  };
+
+  const updateTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
+          : todo
+      )
+    );
+  };
+
+  const handleChangeText = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log('input:' + input);
+  }, [input]);
+
+  return (
+    <div className='App' style={wrap}>
+      <h1>TODO LIST</h1>
+      <form onSubmit={insertTodo}>
+        <input
+          type='text'
+          required={true}
+          value={input}
+          onChange={handleChangeText}
+        />
+        <input type='submit' value='Create' />
+      </form>
+      {todos
+        ? todos.map((todo) => {
+            return (
+              // list를 출력할때는 각 값의 고유한 값을 줘야한다
+              <div className='todo' key={todo.id}>
+                <h3>
+                  <label
+                    className={todo.completed ? 'completed' : null}
+                    onClick={() => updateTodo(todo.id)}
+                  >
+                    {todo.todoname}
+                  </label>
+                </h3>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 }
 
 export default App;
