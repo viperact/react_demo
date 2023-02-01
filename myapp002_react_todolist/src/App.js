8241c7958f14d54f3ef34436abd604318f444ce0
viperact <viperact26@gmail.com>
2023-02-01 21:51:12 +0900

    update

--- myapp002_react_todolist/src/App.js
+++ myapp002_react_todolist/src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const wrap = {
+    width: '500px',
+    border: '1px solid black',
+    margin: '10px auto',
+  };
+
+  const baseUrl = 'http://localhost:8090/myapp';
+
+  let boardList = [
+    { id: 1, todoname: '운동하기', completed: 0 },
+    { id: 2, todoname: 'SNS꾸미기', completed: 0 },
+    { id: 3, todoname: '사진정리하기', completed: 0 },
+  ];
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  function getTodos() {
+    //db접근해 가져오는 작업
+  }
+
+  const insertTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      { id: todos.length + 1, todoname: input, completed: 0 },
+      ...todos,
+    ]);
+
+    setInput(''); // 입력란 초기화
+  };
+
+  const updateTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
+          : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    // setTodos(todos.filter((todo) => todo.id !== id));
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id; //처리하는부분이 하나일땐 {return} 생략가능
+      })
+    );
+  };
+
+  const handleChangeText = (e) => {
+    setInput(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log('input:' + input);
+  }, [input]);
+
+  // JSX로 사용하는 부분
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={wrap}>
+      <h1>TODO LIST</h1>
+      <form onSubmit={insertTodo}>
+        <input
+          type='text'
+          required={true}
+          value={input}
+          onChange={handleChangeText}
+        />
+        <input type='submit' value='Create' />
+      </form>
+      {todos
+        ? todos.map((todo) => {
+            return (
+              // list를 출력할때는 각 값의 고유한 값을 줘야한다
+              <div className='todo' key={todo.id}>
+                <h3>
+                  <label
+                    className={todo.completed ? 'completed' : null}
+                    onClick={() => updateTodo(todo.id)}
+                  >
+                    {todo.todoname}
+                  </label>
+
+                  <label
+                    onClick={() => {
+                      deleteTodo(todo.id);
+                    }}
+                  >
+                    &nbsp;&nbsp;삭제
+                  </label>
+                </h3>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
---
+++ myapp002_react_todolist/src/commonApi/todoApi.js
@@ -0,0 +1,3 @@
+const baseUrl = 'http://localhost:8090/myapp';
+
+export { baseUrl };
